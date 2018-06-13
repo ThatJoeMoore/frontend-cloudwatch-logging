550383a0faba6988142be2e3b84323aee02bdf1d
Scott Hutchings <yoshutch@gmail.com>
2018-06-13 16:34:18 -0600

    lambda logger parses input and prints the logs to the lambda console

--- backend/handel-extension/src/lambda/lambda-logger.py
+++ backend/handel-extension/src/lambda/lambda-logger.py
@@ -1,11 +1,43 @@
 
 def handler(event, context):
 
+    logs = []
+    if 'Content-Type' in event['headers'] and event['headers']['Content-Type'] == 'text':
+        log_lines = event['body'].split('\n')
+        for line in log_lines:
+            line_pieces = line.split('\t')
+            parts = []
+            for arg in line_pieces[2:]:
+                if arg.startswith('{str}'):
+                    parts.append({"string": arg[5:].replace('"', '')})
+                elif arg.startswith('{err}'):
+                    parts.append({"error": arg[5:].replace('"', '')})
+                elif arg.startswith('{obj}'):
+                    parts.append({"object": arg[5:].replace('"', '')})
+            logs.append({
+                "timestamp": line_pieces[0],
+                "level": line_pieces[1],
+                "parts": parts
+            })
+    elif 'Content-Type' in event['headers'] and event['headers']['Content-Type'] == 'application/json':
+        logs = event['body']
+
+    for log in logs:
+        log_string = "{}\t{}".format(log['timestamp'], log['level'])
+        for part in log['parts']:
+            if 'string' in part:
+                log_string += "\t{}".format(part['string'])
+            elif 'object' in part:
+                log_string += "\t{}".format(part['object'])
+            elif 'error' in part:
+                log_string += "\t{}".format(part['error'])
+        print(log_string)
+
     return {
-        "isBase64Encoded": False,
-        "statusCode": 200,
-        "headers": {},
-        "body": {
-            "message": "Hello from Lambda!"
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {},
+        'body': {
+            'message': 'Hello from Lambda!'
         }
     }
